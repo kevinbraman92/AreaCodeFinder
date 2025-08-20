cca0ca869c6919ccb993803ba729be785635b4ee
Kevin <128657573+kevinbraman92@users.noreply.github.com>
2025-08-20 14:31:21 -0500

    Update AreaCodeFinder.py
    Refactored Code for neatness

--- scripts/AreaCodeFinder.py
+++ scripts/AreaCodeFinder.py
@@ -1,13 +1,13 @@
 import os
-import sys
 import pandas as pd
 from pathlib import Path
 from datetime import datetime
 
 DATE_STR = datetime.now().strftime('%m.%d.%y')
 ROOT = Path(__file__).resolve().parent.parent
-INPUT_CSV= ROOT / "input" / 'input.csv'
+INPUT_CSV = ROOT / "input" / 'input.csv'
 REPORT_DIR = ROOT / "report" / 'npa_report.csv'
+
 
 def inputFileValidator():
     if not os.path.exists('input.xlsx'):
@@ -15,36 +15,40 @@ def inputFileValidator():
         df = pd.DataFrame({'Phone': ['123-456-7890']})
         df.to_excel('input.xlsx', index=False, engine='openpyxl')
         print("Created 'input.xlsx'. Please fill in your phone numbers and re-run the script.")
-        exit() 
+        exit()
 
-def areaCodeFinder():
 
+def main():
+    inputFileValidator()
     print("Starting script...")
 
-    #Open Files
+    # Open Files
     input_df = pd.read_csv(INPUT_CSV, low_memory=False)
     npa_report_df = pd.read_csv(REPORT_DIR, skiprows=1, low_memory=False)
 
-    #Correcting Input Header
+    # Correcting Input Header
     input_df.columns = input_df.columns.str.strip()
     if input_df.columns[0] != 'Phone':
         print(f"Warning: First column was '{input_df.columns[0]}', renaming to 'Phone'")
         input_df.columns.values[0] = 'Phone'
 
-    #Clean Input
+    # Clean Input
     input_df['CleanPhone'] = input_df['Phone'].astype(str).str.replace(r'\D', '', regex=True)
     input_df['CleanPhone'] = input_df['CleanPhone'].apply(lambda x: x[1:] if len(x) == 11 and x.startswith('1') else x)
     input_df['AreaCode'] = input_df['CleanPhone'].astype(str).str[:3]
 
-    #Clean Primary Key
+    # Clean Primary Key
     npa_report_df['NPA_ID'] = npa_report_df['NPA_ID'].astype(str)
 
-    #Perform Lookup
+    # Preform Lookup
     merged_df = input_df.merge(npa_report_df[['NPA_ID', 'LOCATION', 'COUNTRY', 'IN_SERVICE']], left_on='AreaCode', right_on='NPA_ID', how='left')
 
-    #Write File As CSV
+    # Write File As CSV
     merged_df.to_csv(f'output {DATE_STR}.csv', index=False)
 
-    #Script Complete
-
+    # Script Complete
     print(f"Script complete! File save as 'output {DATE_STR}.csv'.")
+
+
+if __name__ == "__main__":
+    main()
